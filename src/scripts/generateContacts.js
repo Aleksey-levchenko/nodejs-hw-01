e0a2a01ebc1f16c77c6343293bc33e42b61d82ec
Oleksiy Levchenko <aleksej.levchenko93@gmail.com>
2025-06-01 00:24:16 +0300

    HW-01 complete

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,16 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+    const newContacts = Array.from({ length: number }, createFakeContact);
+    const updatedContacts = contacts.concat(newContacts);
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.error('Помилка при генерації контактів: ', error.message);
+  }
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,21 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (contacts.length === 0) {
+      console.log('Контактів немає, нічого видаляти.');
+      return;
+    }
+
+    const deleteLastContact = contacts.pop();
+    await writeContacts(contacts);
+    return deleteLastContact;
+  } catch (error) {
+    console.error('Помилка при видаленні контакта: ', error.message);
+  }
+};
 
 removeLastContact();
